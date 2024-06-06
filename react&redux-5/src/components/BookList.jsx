7f47c949c7b77a1c5193f7c4787d123898107e07
AnB School 0012 <anbschool0012@AnBs-Mac-mini-77.local>
2024-06-06 19:23:20 +0700

    context react.

--- react&redux-5/src/components/BookList.jsx
+++ react&redux-5/src/components/BookList.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return <div className="grid grid-cols-3 gap-4">{renderedBooks}</div>;
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {renderedBooks} {value}
+    </div>
+  );
 };
 
 export default BookList;
