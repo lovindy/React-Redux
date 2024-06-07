91a084ea5f4feaeb2da20ab5f26d3b0c5f4eb795
AnB School 0012 <anbschool0012@AnBs-Mac-mini-77.local>
2024-06-07 18:42:37 +0700

    clean code.

--- react&redux-6/src/App.jsx
+++ react&redux-6/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
-const App = () => {
+import React from "react";
+import Button from "./components/Button";
+import { GoBell } from "react-icons/go";
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="gap-4 fixed inset-0 flex items-center justify-center">
+      <div>
+        <Button warning rounded outline>
+          <GoBell />
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded>
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
