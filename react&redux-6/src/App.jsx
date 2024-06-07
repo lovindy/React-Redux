310af6ae27adf7aac0f60ee8acfb5047f37fbd97
AnB School 0012 <anbschool0012@AnBs-Mac-mini-77.local>
2024-06-07 15:58:58 +0700

    new project.

--- react&redux-6/src/App.jsx
+++ react&redux-6/src/App.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect, useContext } from "react";
-import BookCreate from "./components/BookCreate";
-import BookList from "./components/BookList";
-import BooksContext from "./context/books";
+import React from 'react'
+
 const App = () => {
-  const { fetchBooks } = useContext(BooksContext);
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
   return (
-    <div className="max-w-screen-xl mx-auto flex flex-col items-center gap-12 mt-36">
-      <div className="flex justify-center items-center">
-        <BookCreate />
-      </div>
-      <BookList />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
